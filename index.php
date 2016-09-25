b4eaef61ace381b8fee6f5b3063fd6e3ea407113
Liam Sheppard <lsheppard1995@gmail.com>
2016-09-25 20:36:37 +1000

    Added polygon background

--- index.php
+++ index.php
@@ -4,9 +4,17 @@
     <meta charset="utf-8">
     <title>Horizon / QUT Interactive + Visual Design Graduate Exhibition 2016</title>
     <link rel="stylesheet" href="<?= get_stylesheet_directory_uri() ?>/horizon16/css/styles.min.css" media="screen" title="no title">
-    <link rel="shortcut icon" href="<?= get_stylesheet_directory_uri() ?>/favicon.png">
+    <?php include 'php-partials/favicon.php';?>
   </head>
+
     <body>
+
+        <div class='poly-background'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 571.56 365.46"><polyline class="a" points="0.7 339.98 285.8 0.84 387.88 161.37 570.9 232.29"/><polyline class="b" points="570.9 232.29 401.17 310.46 354.43 364.94 0.7 339.98"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 594.4 383.72"><polyline class="a" points="0.37 216.58 254.51 0.7 594.03 383.22"/><polyline class="b" points="594.03 383.22 118.57 349.7 0.37 216.58"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 594.3 409.59"><polyline points="593.91 216.72 339.77 0.83 200.32 100.74 0.26 383.36" class="a"></polyline><polyline class="b" points="0.26 383.36 240.07 370.99 403.81 400.99 593.91 216.72"></polyline></svg>
+        </div>
+
         <header id="site-header">
             <a href="<?= get_bloginfo('url') ?>" class="horizon-glyph toast-is-ready">
                 <span class="horizon-glyph-glyph"></span>
