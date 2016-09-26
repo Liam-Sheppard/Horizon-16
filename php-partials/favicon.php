507852d328d7d2727f800d6338212c5919904e3a
Liam Sheppard <lsheppard1995@gmail.com>
2016-09-26 22:55:49 +1000

    :bug: Favicon fix.

--- php-partials/favicon.php
+++ php-partials/favicon.php
@@ -1,8 +1,8 @@
-<link rel="apple-touch-icon" sizes="180x180" href="<?= get_stylesheet_directory_uri() ?>/horizon16/images/favicons/favicon.png/apple-touch-icon.png">
-<link rel="icon" type="image/png" href="<?= get_stylesheet_directory_uri() ?>/horizon16/images/favicons/favicon.png/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="<?= get_stylesheet_directory_uri() ?>/horizon16/images/favicons/favicon.png/favicon-16x16.png" sizes="16x16">
-<link rel="manifest" href="<?= get_stylesheet_directory_uri() ?>/horizon16/images/favicons/favicon.png/manifest.json">
-<link rel="mask-icon" href="<?= get_stylesheet_directory_uri() ?>/horizon16/images/favicons/favicon.png/safari-pinned-tab.svg" color="#111111">
-<link rel="shortcut icon" href="<?= get_stylesheet_directory_uri() ?>/horizon16/images/favicons/favicon.png/favicon.ico">
-<meta name="msapplication-config" content="<?= get_stylesheet_directory_uri() ?>/horizon16/images/favicons/favicon.png/browserconfig.xml">
+<link rel="apple-touch-icon" sizes="180x180" href="<?= get_stylesheet_directory_uri() ?>/horizon16/images/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="<?= get_stylesheet_directory_uri() ?>/horizon16/images/favicons/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="<?= get_stylesheet_directory_uri() ?>/horizon16/images/favicons/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="<?= get_stylesheet_directory_uri() ?>/horizon16/images/favicons/manifest.json">
+<link rel="mask-icon" href="<?= get_stylesheet_directory_uri() ?>/horizon16/images/favicons/safari-pinned-tab.svg" color="#111111">
+<link rel="shortcut icon" href="<?= get_stylesheet_directory_uri() ?>/horizon16/images/favicons/favicon.ico">
+<meta name="msapplication-config" content="<?= get_stylesheet_directory_uri() ?>/horizon16/images/favicons/browserconfig.xml">
 <meta name="theme-color" content="#111111">
