d439957bb837cd0ae79251f2437088032bde7271
Liam Sheppard <lsheppard1995@gmail.com>
2016-10-09 23:16:14 +1000

    Changed href='#' to js:void(0) on homepage grad profiles.

--- php-partials/graduates-landing.php
+++ php-partials/graduates-landing.php
@@ -8,27 +8,27 @@
 
   <ul class='strip-container'>
     <li class='single-grad'>
-      <a href='#' class='grad-container'>
+      <a href='javascript:void(0)' class='grad-container'>
         <img src='<?= get_stylesheet_directory_uri() ?>/horizon16/images/temp-grad-1.png'></img>
         <span class='grad-name'>Mark Lappin</span>
       </a>
     </li><li class='single-grad'>
-      <a href='#' class='grad-container'>
+      <a href='javascript:void(0)' class='grad-container'>
         <img src='<?= get_stylesheet_directory_uri() ?>/horizon16/images/temp-grad-2.png'></img>
         <span class='grad-name'>Michael Schmidt</span>
       </a>
     </li><li class='single-grad'>
-      <a href='#' class='grad-container'>
+      <a href='javascript:void(0)' class='grad-container'>
         <img src='<?= get_stylesheet_directory_uri() ?>/horizon16/images/temp-grad-3.png'></img>
         <span class='grad-name'>Matt Keliher</span>
       </a>
     </li><li class='single-grad'>
-      <a href='#' class='grad-container'>
+      <a href='javascript:void(0)' class='grad-container'>
         <img src='<?= get_stylesheet_directory_uri() ?>/horizon16/images/temp-grad-4.png'></img>
         <span class='grad-name'>Olivia McKenna</span>
       </a>
     </li><li class='single-grad'>
-      <a href='#' class='grad-container'>
+      <a href='javascript:void(0)' class='grad-container'>
         <img src='<?= get_stylesheet_directory_uri() ?>/horizon16/images/temp-grad-1.png'></img>
         <span class='grad-name'>Mark Lappin</span>
       </a>
