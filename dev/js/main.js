d70bab8f2e582f27759cb253a91af07aebc332a0
Liam Sheppard <lsheppard1995@gmail.com>
2016-10-12 01:27:45 +1000

    Added another grad.

--- dev/js/main.js
+++ dev/js/main.js
@@ -133,7 +133,7 @@ $(document).ready( function() {
         gradUnique,
         newGrad,
         i = 0,
-        numberOfGrads = 24,
+        numberOfGrads = 26,
         gradsJSON;
     $.ajax({
       type: 'GET',
