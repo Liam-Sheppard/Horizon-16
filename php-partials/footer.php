deb54752d29c4c12cc91fe35c8b6888431997ad9
Liam Sheppard <lsheppard1995@gmail.com>
2016-10-11 20:57:08 +1000

    horzion -> horizon

--- php-partials/footer.php
+++ php-partials/footer.php
@@ -4,6 +4,6 @@
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 594.4 383.72" class="polygon layer" data-desktop-depth="0.2" data-mobile-depth="1" data-depth="0.3"><polyline class="a" points="0.37 216.58 254.51 0.7 594.03 240"/><polyline class="b" points="594.03 240 200 349.7 0.37 216.58"/></svg>
   </div>
   */ ?>
-  <a href="mailto:hello@horizon16.com" class="fancy-link">hello@horzion16.com</a>
+  <a href="mailto:hello@horizon16.com" class="fancy-link">hello@horizon16.com</a>
   <?php partial('social-media', ['show_hashtag' => false]); ?>
 </footer>
