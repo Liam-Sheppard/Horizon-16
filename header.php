73798360e8f5cde6f58d5e6d9689677e1101c0a7
Michael Schmidt <me@michaelschmidt.com.au>
2016-10-11 16:14:00 +1000

    OG image setting

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta property="og:url"                content="https://www.horizon16.com" />
     <meta property="og:title"              content="Horizon 2016" />
     <meta property="og:description"        content="QUT Interactive + Visual Design Graduate Exhibition 2016" />
-    <meta property="og:image"              content="<?= get_stylesheet_directory_uri() ?>/assets/images/og-horizon16.png" />
+    <meta property="og:image"              content="<?= get_stylesheet_directory_uri() ?>/assets/images/og-horizon.png" />
     <?php partial('favicon'); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script type='text/javascript' src='<?= get_stylesheet_directory_uri() ?>/assets/js/jquery-3.1.0.min.js'></script>
