fb161813b9e63fd1e410d437143bed232376288a
Liam Sheppard <lsheppard1995@gmail.com>
2016-10-12 17:11:54 +1000

    Started graduates page.

---
+++ php-partials/graduate.php
@@ -0,0 +1,8 @@
+<li class='single-grad'>
+  <a href='javascript:void(0)' class='grad-container'>
+    <img src='<?= get_stylesheet_directory_uri() ?>/assets/images/temp-grad-1.png'></img>
+    <span class='grad-name'>
+      <?php echo $name ?>
+    </span>
+  </a>
+</li>
