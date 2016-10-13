400fb5e502a0e0f5c3400198e3698da6efd8c4dd
Michael <me@michaelschmidt.com.au>
2016-10-13 15:50:55 +1000

    Add meow egg

--- dev/js/easter-eggs.js
+++ dev/js/easter-eggs.js
@@ -15,7 +15,23 @@
         // console.log(this.activeEgg.metadata);
       }).listen();
 
+    egg
+      .addCode("m,e,o,w", function() {
+        $('body').prepend('<div class="holly"></div>');
+        setTimeout(function(){
+          $('.holly').fadeout();
+        }, 5500);
+        setTimeout(function(){
+          $('.holly').remove();
+        }, 7500);
+      })
+      .addHook(function(){
+        // console.log("Hook called for: " + this.activeEgg.keys);
+        // console.log(this.activeEgg.metadata);
+      }).listen();
+
       console.log('Easter Egg Code: v o v o');
+      console.log('Easter Egg Code: m e o w');
   });
 
 })(jQuery);
