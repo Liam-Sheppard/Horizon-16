9f0ea569442d587c60dc1e9f48b668eefd401646
Liam Sheppard <lsheppard1995@gmail.com>
2016-10-13 20:07:01 +1000

    Idk what i did in this commit lol

--- php-partials/graduate.php
+++ php-partials/graduate.php
@@ -1,6 +1,6 @@
 <li class='single-grad'>
   <a href='javascript:void(0)' class='grad-container'>
-    <img src='<?= get_stylesheet_directory_uri() ?>/assets/images/temp-grad-1.png'></img>
+    <div class='sprite'></div>
     <span class='grad-name'>
       <?php echo $name ?>
     </span>
