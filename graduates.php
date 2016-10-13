de8bbeab0a932649f7eb4232ff14da033ace95f6
Michael <me@michaelschmidt.com.au>
2016-10-13 23:35:23 +1000

    Trying horizontal scroll for grads

--- graduates.php
+++ graduates.php
@@ -12,18 +12,51 @@ get_header();
 partial('header');
 
 ?>
+
 <div class="panel panel--graduate-strips">
-  <ul class='strip-container'>
+  <div class="graduates-container">
 
-    <?php
-    for ($x = 0; $x <= 46; $x++) {
-      partial('graduate', [
-        'name' => 'Michael Schmidt',
-      ]);
-    }
-    ?>
+    <h1 class="h2">Graduates</h1>
 
-  </ul>
+    <div class="strips-container">
+      <div class="strip">
+          <?php
+          for ($x = 0; $x <= 16; $x++) {
+            partial('graduate', [
+              'name' => 'Michael Schmidt',
+              'image' => get_stylesheet_directory_uri() . '/assets/images/graduate-images-600x400/bae-haram.png',
+            ]);
+          }
+          ?>
+      </div>
+      <div class="strip">
+          <?php
+          for ($x = 0; $x <= 16; $x++) {
+            partial('graduate', [
+              'name' => 'Michael Schmidt',
+              'image' => get_stylesheet_directory_uri() . '/assets/images/graduate-images-600x400/bae-haram.png',
+            ]);
+          }
+          ?>
+      </div>
+      <div class="strip">
+          <?php
+          for ($x = 0; $x <= 13; $x++) {
+            partial('graduate', [
+              'name' => 'Michael Schmidt',
+              'image' => get_stylesheet_directory_uri() . '/assets/images/graduate-images-600x400/bae-haram.png',
+            ]);
+          }
+          ?>
+      </div>
+    </div>
+
+  </div>
+
+
+
+
+
 </div>
 
 <?php
