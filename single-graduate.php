eaf36a9491487ec0c5a2196d6e5510599c30b44a
Liam Sheppard <lsheppard1995@gmail.com>
2016-10-14 00:33:08 +1000

    Started single graduate page.

---
+++ single-graduate.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* Template Name: Single Graduate
+*
+*/
+?>
+
+<?php
+
+get_header();
+
+partial('header');
+
+?>
+
+<div class="panel panel--single-graduate">
+
+  <div class='left-pane grad-1'>
+    <div class='grad-image'></div>
+  </div>
+
+</div>
+
+
+<?php
+
+//partial('footer');
+
+get_footer();
+
+?>
