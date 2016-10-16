ca5113fc95427ae6de8ce3437f6809a4a4807336
Liam Sheppard <lsheppard1995@gmail.com>
2016-10-16 23:21:00 +1000

    Making sure generated files are up to date.

--- dev/js/main.js
+++ dev/js/main.js
@@ -80,65 +80,31 @@ $(document).ready( function() {
   console.log(siteData.graduates);
 
     // Homepage Graduates
-    var $famID = $('#homepageFamJS'),
-        imagePath = siteData.themeUri + '/assets/images/graduate-images-600x400/',
-        prevGrads,
-        gradUnique,
-        newGrad,
-        i = 0,
-        numberOfGrads = siteData.graduates.length,
-        gradsJSON = siteData.graduates,
-        gradRandom = function (min, max){
-          return Math.floor((Math.random() * max) + min);
-        };
-
-
-
-        var currentGrads = [];
-        for (i; i < 6; i++) {
-          gradUnique = false;
-          while (!gradUnique) {
-            newGrad = gradRandom(0, numberOfGrads - 1);
-            gradUnique = checkDupes(newGrad, currentGrads);
-          }
-          currentGrads[i] = newGrad;
-          var grad = gradsJSON[currentGrads[i]];
-          $famID.append('<li class="single-grad active"><div class="grad-container"><img src="' + siteData.themeUri + '/assets/images/graduate-images-600x400/graduate-' + grad.ID + '.png"><span class="grad-name"><span class="board"></span><span class="name">' + grad.full_name + '</span></span></div></li>');
-          prevGrads = currentGrads;
-        }
-
-
-
-    // Reshuffle Graduates
-    // function shuffleFam(){
-    //   $('.grad-container img').fadeTo(300, 0);
-    //   $('.grad-container .grad-name').fadeTo(300, 0).promise().done(function() {
-    //     var currentGrads = [];
-    //     var gradNew;
-    //     i = 0;
-    //     for (i; i < 6; i++) {
-    //       var loop = true;
-    //       gradUnique = false;
-    //       gradNew = false;
+    // var $famID = $('#homepageFamJS'),
+    //     imagePath = siteData.themeUri + '/assets/images/graduate-images-600x400/',
+    //     prevGrads,
+    //     gradUnique,
+    //     newGrad,
+    //     i = 0,
+    //     numberOfGrads = siteData.graduates.length,
+    //     gradsJSON = siteData.graduates;
     //
-    //       while ( loop ) {
-    //         newGrad = gradRandom(0, numberOfGrads - 1);
-    //         gradUnique = checkDupes(newGrad, currentGrads);
-    //         gradNew = checkDupes(newGrad, prevGrads);
-    //         if ( gradUnique && gradNew ) {
-    //           loop = false;
-    //         }
-    //       }
-    //       currentGrads[i] = newGrad;
-    //
-    //       var grad = siteData.graduates[currentGrads[i]];
-    //       $('#homepageFamJS').children(".single-grad:nth-of-type(" + ( i + 1 ) + ")").children().html("<img src='" + siteData.themeUri + "/assets/images/graduate-images-600x400/graduate-" + grad.ID + ".png'><span class='grad-name'><span class='board'></span><span class='name'>" + grad.full_name + "</span></span>");
-    //     }
-    //     prevGrads = currentGrads;
-    //   });
+    // function gradRandom(min, max) {
+    //   return Math.floor((Math.random() * max) + min);
     // }
-    // if($('#homepageFamJS').length){
-    //   setInterval(shuffleFam, 5000);
+    //
+    // var currentGrads = [];
+    // for (i; i < 6; i++) {
+    //   gradUnique = false;
+    //   while (!gradUnique) {
+    //     newGrad = gradRandom(0, numberOfGrads - 1);
+    //     gradUnique = checkDupes(newGrad, currentGrads);
+    //   }
+    //   currentGrads[i] = newGrad;
+    //   var grad = gradsJSON[currentGrads[i]];
+    //   console.log('test' + i);
+    //   $famID.append('<li class="single-grad active" attr="' + i + '"><div class="grad-container"><img src="' + siteData.themeUri + '/assets/images/graduate-images-600x400/graduate-' + grad.ID + '.png"><span class="grad-name"><span class="board"></span><span class="name">' + grad.full_name + '</span></span></div></li>');
+    //   prevGrads = currentGrads;
     // }
 
 
@@ -147,14 +113,14 @@ $(document).ready( function() {
     Returns false if grad is repeated.
     Returns true if grad is unique.
     */
-    function checkDupes(n, currentGrads){
-      var i = 0;
-      for ( i; i < currentGrads.length; i++ ) {
-        if ( currentGrads[i] == n ) {
-          // console.log('conflict');
-          return false;
-        }
-      }
-      return true;
-    }
+    // function checkDupes(n, currentGrads){
+    //   var i = 0;
+    //   for ( i; i < currentGrads.length; i++ ) {
+    //     if ( currentGrads[i] == n ) {
+    //       // console.log('conflict');
+    //       return false;
+    //     }
+    //   }
+    //   return true;
+    // }
 });
