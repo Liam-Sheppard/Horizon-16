e36a2dd558b0e3a4525762396afb6fa68478be63
Liam Sheppard <lsheppard1995@gmail.com>
2016-10-18 11:45:51 +1000

    I am not a smart person.

--- php-partials/graduate.php
+++ php-partials/graduate.php
@@ -1,5 +1,5 @@
 <li class="single-grad active <?= isset($disciplines) ? implode(' ', $disciplines) : false ?>">
-  <a href="<?= isset($permalink) ? $permalink : '<?= get_home_url() ?>/graduates' ?>" class="grad-container">
+  <a href="<?= isset($permalink) ? $permalink : get_home_url() + '/graduates' ?>" class="grad-container">
     <img src="<?= $image ?>">
     <span class='grad-name'><span class="board"></span><span class="name"><?= $name ?></span></span>
   </a>
