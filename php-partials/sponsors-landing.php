094cd4972b768aef28d081bdd335bfaf81dc3626
TomJC <thomas_185@hotmail.com>
2016-10-27 17:30:01 +1000

    still working on it....

--- php-partials/sponsors-landing.php
+++ php-partials/sponsors-landing.php
@@ -2,17 +2,17 @@
   <h2 class="seen">Made possible by</h2>
   <div id="sponsor-images-big">
     <p>
-      <a href="http://www.ciprecinct.qut.edu.au/" target="_blank">"QUTCIP"<img src="https://horizon16.com/wp-content/themes/horizon_v2.1/assets/images/logos/QUTCI_Logo_TEMPORARY.png" width="450" height="180"</a>
-      <a href="http://www.croomo.com/" target="_blank">"CROOMO"<img src="https://horizon16.com/wp-content/themes/horizon_v2.1/assets/images/logos/croomo-white-2.png" width="450" height="180"</a>
-      <a href="http://www.vision6.com.au/" target="_blank">"VISION6"<img src="https://horizon16.com/wp-content/themes/horizon_v2.1/assets/images/logos/vision6_logo_white.PNG" width="450" height="180"</a>
+      <a href="http://www.ciprecinct.qut.edu.au/" target="_blank"><img src="https://horizon16.com/wp-content/themes/horizon_v2.1/assets/images/logos/QUTCI_Logo_TEMPORARY.png" width="450" height="130"</a>
+      <a href="http://www.croomo.com/" target="_blank"><img src="https://horizon16.com/wp-content/themes/horizon_v2.1/assets/images/logos/croomo-white-2.png" width="450" height="130"</a>
+      <a href="http://www.vision6.com.au/" target="_blank"><img src="https://horizon16.com/wp-content/themes/horizon_v2.1/assets/images/logos/vision6_logo_white.png" width="450" height="130"</a>
     </p>
   </div>
   <div id="sponsor-images-small">
     <p>
-      <a href="https://www.tanda.co/" target="_blank">"TANDA"<img src="https://horizon16.com/wp-content/themes/horizon_v2.1/assets/images/logos/tanda_logo-white.png" width="86" height="26"</a>
-      <a href="http://qutcea.com/" target="_blank">"CEA"<img src="https://horizon16.com/wp-content/themes/horizon_v2.1/assets/images/logos/CEA-Logo-white.png" width="86" height="26"</a>
-      <a href="http://www.emperorcreative.com.au/" target="_blank">"EMPEROR"<img src="https://horizon16.com/wp-content/themes/horizon_v2.1/assets/images/logos/emperor.png" width="86" height="26"</a>
-      <a href="http://www.capral.com.au/" target="_blank">"CAPRAL"<img src="https://horizon16.com/wp-content/themes/horizon_v2.1/assets/images/logos/logo_header_capral2.png" width="86" height="26"</a>
+      <a href="https://www.tanda.co/" target="_blank"><img src="https://horizon16.com/wp-content/themes/horizon_v2.1/assets/images/logos/tanda_logo-white.png" width="86" height="26"</a>
+      <a href="http://qutcea.com/" target="_blank"><img src="https://horizon16.com/wp-content/themes/horizon_v2.1/assets/images/logos/CEA-Logo-white.png" width="86" height="26"</a>
+      <a href="http://www.emperorcreative.com.au/" target="_blank"><img src="https://horizon16.com/wp-content/themes/horizon_v2.1/assets/images/logos/emperor.png" width="86" height="26"</a>
+      <a href="http://www.capral.com.au/" target="_blank"><img src="https://horizon16.com/wp-content/themes/horizon_v2.1/assets/images/logos/logo_header_capral2.png" width="86" height="26"</a>
     </p>
   </div>
 </div>
