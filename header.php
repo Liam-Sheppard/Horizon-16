939ecce418395b8820a17de89f16ab1e64376a18
Liam Sheppard <lsheppard1995@gmail.com>
2016-10-30 15:28:40 +1000

    Added analytics

--- header.php
+++ header.php
@@ -10,6 +10,9 @@
     <?php partial('favicon'); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script type='text/javascript' src='<?= get_stylesheet_directory_uri() ?>/assets/js/jquery-3.1.0.min.js'></script>
-    <?php wp_head() ?>
+    <?php
+    wp_head()
+    partial('google-analytics');
+    ?>
   </head>
     <body <?php body_class() ?>>
