2223fca38c7e2c01e22cfce0d249399687dd93b7
Liam Sheppard <lsheppard1995@gmail.com>
2016-10-31 00:52:46 +1000

    Made sponsors reSPONSive hehehehhehehehe

--- php-partials/sponsors-landing.php
+++ php-partials/sponsors-landing.php
@@ -2,15 +2,15 @@
 
 <div class="panel panel--sponsors">
   <h2>Meet Our Beloved Sponsors</h2>
-  <div id="sponsor-images-big">
-      <a href="http://www.ciprecinct.qut.edu.au/" target="_blank"><img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/QUTCI_Logo_TEMPORARY.png"></img></a>
-      <a href="http://www.croomo.com/" target="_blank"><img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/croomo-white-2.png"></img></a>
-      <a href="http://www.vision6.com.au/" target="_blank"><img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/vision6_logo_white.png"></img></a>
+  <div class='sponsors-gold'>
+      <a href="http://www.ciprecinct.qut.edu.au/" target="_blank" class='sponsor sponsor-qutcip'>Creative Industries Precinct<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/QUTCI_Logo_TEMPORARY.png"></img></a>
+      <a href="http://www.croomo.com/" target="_blank" class='sponsor sponsor-croomo'>Croomo<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/croomo-white-2.png"></img></a>
+      <a href="http://www.vision6.com.au/" target="_blank" class='sponsor sponsor-vision6'>Vision6<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/vision6_logo_white.png"></img></a>
   </div>
-  <div id="sponsor-images-small">
-      <a href="https://www.tanda.co/" target="_blank"><img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/tanda_logo-white.png"></img></a>
-      <a href="http://qutcea.com/" target="_blank"><img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/CEA-Logo-white.png"></img></a>
-      <a href="http://www.emperorcreative.com.au/" target="_blank"><img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/emperor.png"></img></a>
-      <a href="http://www.capral.com.au/" target="_blank"><img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/logo_header_capral2.png"></img></a>
+  <div class="sponsors-silver-bronze">
+      <a href="https://www.tanda.co/" target="_blank" class='sponsor sponsor-tanda'>Tanda<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/tanda_logo-white.png"></img></a>
+      <a href="http://qutcea.com/" target="_blank" class='sponsor sponsor-qutcea'>QUT Creative Enterprises Australia<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/CEA-Logo-white.png"></img></a>
+      <a href="http://www.emperorcreative.com.au/" target="_blank" class='sponsor sponsor-emperor'>Emperor Creative<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/emperor.png"></img></a>
+      <a href="http://www.capral.com.au/" target="_blank" class='sponsor sponsor-capral'>Capral<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/logo_header_capral2.png"></img></a>
   </div>
 </div>
