1875939eb46e6935fe1e50a3692714dd4abf2481
TomJC <thomas_185@hotmail.com>
2016-10-30 22:46:07 +1000

    vision6 img wasn't loading

--- php-partials/sponsors-landing.php
+++ php-partials/sponsors-landing.php
@@ -5,7 +5,7 @@
   <div id="sponsor-images-big">
       <a href="http://www.ciprecinct.qut.edu.au/" target="_blank"><img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/QUTCI_Logo_TEMPORARY.png"></img></a>
       <a href="http://www.croomo.com/" target="_blank"><img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/croomo-white-2.png"></img></a>
-      <a href="http://www.vision6.com.au/" target="_blank"><img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/vision6_logo_white.png"></img></a>
+      <a href="http://www.vision6.com.au/" target="_blank"><img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/vision6_logo_white.PNG"></img></a>
   </div>
   <div id="sponsor-images-small">
       <a href="https://www.tanda.co/" target="_blank"><img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/tanda_logo-white.png"></img></a>
