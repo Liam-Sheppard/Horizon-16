fe52df919c8d6c221ae89b847fb46f16e7c3611f
Michael <me@michaelschmidt.com.au>
2016-11-04 00:03:35 +1000

    Comment out meta tags for now until I finish

--- header.php
+++ header.php
@@ -4,21 +4,24 @@
     <meta charset="utf-8">
     <title>Horizon / QUT Interactive + Visual Design Graduate Exhibition 2016</title>
     <meta property="og:url"                content="https://www.horizon16.com" />
+    <meta property="og:image" content="<?= get_stylesheet_directory_uri() ?>/assets/images/og-horizon.png" />
+    <meta property="og:title" content="Horizon 16 | QUT Interactive + Visual Design Graduate Exhibition 2016" />
+    <meta property="og:description" content="QUT Interactive + Visual Design Graduate Exhibition 2016" />
+    <meta name="description"        content="QUT Interactive + Visual Design Graduate Exhibition 2016">
     <?php
-
+    /*
+    For when I get around to thisx
     if (is_home()) {
 
       echo '<meta property="og:title" content="Horizon 16 | QUT Interactive + Visual Design Graduate Exhibition 2016" />';
       echo '<meta property="og:description" content="QUT Interactive + Visual Design Graduate Exhibition 2016" />';
       echo '<meta name="description"        content="QUT Interactive + Visual Design Graduate Exhibition 2016">';
-      echo '<meta property="og:image" content="<?= get_stylesheet_directory_uri() ?>/assets/images/og-horizon.png" />';
 
     } else if (is_page_template('graduates.php')) {
 
       echo '<meta property="og:title" content="Horizon 16 | QUT Interactive + Visual Design Graduate Exhibition 2016" />';
       echo '<meta property="og:description" content="Meet the graduating class of QUT Interactive + Visual Design for 2016." />';
       echo '<meta name="description"        content="Meet the graduating class of QUT Interactive + Visual Design for 2016." />';
-      echo '<meta property="og:image" content="<?= get_stylesheet_directory_uri() ?>/assets/images/og-horizon.png" />';
 
     } else if (is_single()) {
 
@@ -27,9 +30,10 @@
     } else if (is_author()) {
 
       echo '<meta property="og:title" content="' . get_the_title() .  ' | QUT Interactive + Visual Design Graduates of 2016" />';
+      var_dump(get_queried_object());
 
     }
-
+    */
     ?>
     <?php partial('favicon'); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
