26e9d1c26f6765e551d472c27a6740b90acb0ef2
Michael <me@michaelschmidt.com.au>
2016-11-09 01:29:27 +1000

    Exhibiton team page

---
+++ php-partials/exhibition-team.php
@@ -0,0 +1,91 @@
+<div class="absolute-polys scene">
+
+  <div class="polygon">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 571.56 365.46" class="polygon layer" data-desktop-depth="0.1" data-mobile-depth="1" data-depth="0.1"><polyline class="a" points="0.7 339.98 285.8 0.84 387.88 161.37 570.9 232.29"/><polyline class="b" points="570.9 232.29 401.17 310.46 354.43 364.94 0.7 339.98"/></svg>
+  </div>
+  <div class="polygon">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 594.4 383.72" class="polygon layer" data-desktop-depth="0.2" data-mobile-depth="1" data-depth="0.3"><polyline class="a" points="0.37 216.58 254.51 0.7 594.03 383.22"/><polyline class="b" points="594.03 383.22 200 349.7 0.37 216.58"/></svg>
+  </div>
+  <div class="polygon">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 594.3 409.59" class="polygon layer" data-desktop-depth="0.03" data-mobile-depth="1" data-depth="0.3"><polyline class="a" points="290.47 226.32 153.24 1.15 0.47 424.32"/><polyline class="b" points="0.47 424.32 238.47 311.82 290.47 226.32"/></svg>
+  </div>
+
+</div>
+<div class="panel panel--exhibition-team">
+
+    <h1 class="site-title">Exhibition Team</h1>
+
+    <div class="team-segment team-segment--left">
+      <div class="team-segment-strip">
+        <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/exhibition-team/horizon-team-coordinator.png" alt="Horizon 16 Takeaways Team" />
+      </div>
+      <div class="team-segment-description">
+        <h2>Captain</h2>
+        <p>
+          The creative genius behind the exhibition theme, and keeper of all the best discount codes known to man, our glorious captain <a href="<?= get_home_url() ?>/graduate/ryanvoevodin/" class="fancy-link">Ryan Voevodin</a> is responsible for guiding the cohort to the finish line with cool-headed leadership and superior team motivation skills.
+        </p>
+      </div>
+    </div>
+
+    <div class="team-segment">
+      <div class="team-segment-strip">
+        <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/exhibition-team/horizon-team-web.png" alt="Horizon 16 Web Team" />
+      </div>
+      <div class="team-segment-description">
+        <h2>Web</h2>
+        <p>
+          From UI/UX design to git version control, to automating staging deployments and manufacture of a handful of beautiful secret easter eggs, the Horizon web team (comprising <a href="<?= get_home_url() ?>/graduate/liamsheppard/" class="fancy-link">Liam Sheppard</a>, <a href="<?= get_home_url() ?>/graduate/michaelschmidt/" class="fancy-link">Michael Schmidt</a> and <a href="<?= get_home_url() ?>/graduate/thomascunningham/" class="fancy-link">Thomas Cunningham</a>) is responsible for making a completely custom experience, true to the Horizon brand.
+        </p>
+      </div>
+    </div>
+
+    <div class="team-segment team-segment--right">
+      <div class="team-segment-strip">
+        <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/exhibition-team/horizon-team-takeaways.png" alt="Horizon 16 Takeaways Team" />
+      </div>
+      <div class="team-segment-description">
+        <h2>Takeaways</h2>
+        <p>
+          Our dedicated takeaways team <a href="<?= get_home_url() ?>/graduate/jessvandersande/" class="fancy-link">Jess Vandersande</a> and <a href="<?= get_home_url() ?>/graduate/charlotteedmondson/" class="fancy-link">Charlotte Edmondson</a> have been responsible for delivering a printed and lasting exhbition 'takeaway' that truly encapsulates the Horizon brand and exhibition experience in the form of an epic cohort card collection.
+        </p>
+      </div>
+    </div>
+
+    <div class="team-segment ">
+      <div class="team-segment-strip">
+        <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/exhibition-team/horizon-team-exhibition.png" alt="Horizon 16 Exhbition Design Team" />
+      </div>
+      <div class="team-segment-description">
+        <h2>Exhibition Design</h2>
+        <p>
+          No exhibition design team could have presented our Horizon 16 IVD Graduates in a better light than our talented <a href="" class="fancy-link">Hannah Saltissi</a> and <a href="" class="fancy-link">Holly Hutson</a>. After countless hours of ideation, planning and management, they transformed the space at QUT's The Block into a true embodiment of the Horizon theme.
+        </p>
+      </div>
+    </div>
+
+    <div class="team-segment">
+      <div class="team-segment-strip">
+        <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/exhibition-team/horizon-team-marketing-video.png" alt="Horizon 16 Video + Motion Graphics Team" />
+      </div>
+      <div class="team-segment-description">
+        <h2>Video + Motion Graphics</h2>
+        <p>
+          Our effects wizard <a href="<?= get_home_url() ?>/graduate/marklappin/" class="fancy-link">Mark Lappin</a> is the creator of Horizon's videography and motion graphics. Applying his multimedia skills to great effect, Mark has brought the Horizon brand to life for promotion and projection, and transformed our team's two-dimensional logo into our three-dimensional light sculpture model.
+        </p>
+      </div>
+    </div>
+
+    <div class="team-segment">
+      <div class="team-segment-strip">
+        <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/exhibition-team/horizon-team-sponsorship.png" alt="Horizon 16 Sponsorship Team" />
+      </div>
+      <div class="team-segment-description">
+        <h2>Sponsorship</h2>
+        <p>
+          Core to the success of the Horizon 16 Graduate Exhibition and its execution, <a href="<?= get_home_url() ?>/graduate/shonarobinson/" class="fancy-link">Shona Robinson</a> and <a href="<?= get_home_url() ?>/graduate/mattkeliher/" class="fancy-link">Matt Keliher </a> were responsible for managing a sponsorship and fundraising campaign targeting both the general public.
+        </p>
+      </div>
+    </div>
+
+
+</div>
--- team.php
+++ team.php
@@ -2,4 +2,13 @@
 /*
 * Template Name: Exhibition Team
 */
-?>
+
+get_header();
+
+partial('header');
+
+partial('exhibition-team');
+
+partial('footer');
+
+get_footer();
