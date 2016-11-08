ed71eaaa6763c90f1503e50b6e23d9733fd865a6
Liam Sheppard <lsheppard1995@gmail.com>
2016-11-08 20:47:35 +1000

    Close work button (Michael make them work pls)

--- single-work.php
+++ single-work.php
@@ -5,7 +5,7 @@ $current_graduate = get_graduates([get_queried_object()->post_author])[0];
 $gallery = get_field('work_gallery');
 ?>
 
-<div class="fixed-polys scene">
+<?php /*<div class="fixed-polys scene">
 
   <div class="polygon">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 594.4 383.72" class="polygon layer" data-desktop-depth="0.2" data-mobile-depth="1" data-depth="0.3"><polyline class="a" points="0.37 216.58 254.51 0.7 594.03 383.22"/><polyline class="b" points="594.03 383.22 200 349.7 0.37 216.58"/></svg>
@@ -14,7 +14,7 @@ $gallery = get_field('work_gallery');
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 594.3 409.59" class="polygon layer" data-desktop-depth="0.03" data-mobile-depth="1" data-depth="0.3"><polyline class="a" points="290.47 226.32 153.24 1.15 0.47 424.32"/><polyline class="b" points="0.47 424.32 238.47 311.82 290.47 226.32"/></svg>
   </div>
 
-</div>
+</div> */ ?>
 
 <div class="work-content">
 
@@ -56,11 +56,22 @@ $gallery = get_field('work_gallery');
           <a href="<?= $project_url ?>" target="_blank" class="button button--black">Visit the project</a>
         </div>
       <?php endif; ?>
-
   </div>
+  <?php if (is_multitouch()) : ?>
+    <div class='mt-close-btn-left mt-close-work'>
+      <div class='mt-close-btn'></div>
+      <span class='mt-close-text'>
+        Close work
+      </span>
+    </div>
+    <div class='mt-close-btn-right mt-close-work'>
+      <span class='mt-close-text'>
+        Close work
+      </span>
+      <div class='mt-close-btn'></div>
+    </div>
+  <?php endif; ?>
 </div>
-
-
 
 <?php
 get_footer();
