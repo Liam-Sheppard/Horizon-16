ce41f5d4db1976fb6a26057be94861c9df3c2b2d
Liam Sheppard <lsheppard1995@gmail.com>
2016-11-08 21:33:01 +1000

    Added TDC sponsor.

--- php-partials/sponsors-landing.php
+++ php-partials/sponsors-landing.php
@@ -9,6 +9,7 @@
   </div>
   <div class="sponsors-silver-bronze">
       <a href="https://www.tanda.co/" target="_blank" class='sponsor sponsor-tanda'>Tanda<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/tanda_logo-white.png"></img></a>
+      <a href="http://thedesignkids.org/" target="_blank" class='sponsor sponsor-qutcea'>The Design Kids<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/tdc.png"></img></a>
       <a href="http://qutcea.com/" target="_blank" class='sponsor sponsor-qutcea'>QUT Creative Enterprises Australia<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/CEA-Logo-white.png"></img></a>
       <a href="http://www.emperorcreative.com.au/" target="_blank" class='sponsor sponsor-emperor'>Emperor Creative<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/emperor.png"></img></a>
       <a href="http://www.capral.com.au/" target="_blank" class='sponsor sponsor-capral'>Capral<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/logos/logo_header_capral2.png"></img></a>
